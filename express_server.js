b5609fd4045978b25278b5d7d356641eb50f7899
houseofsam <samxsaid@gmail.com>
2021-12-06 15:33:41 +0000

    installed and applied EJS to express with instructions from digital ocean tutorial

--- express_server.js
+++ express_server.js
@@ -2,14 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  // res.send('Hello!');
+  res.render('pages/index');
 });
+
+app.get('/about', (req, res) => {
+  const mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  const tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/about', {
+    mascots: mascots,
+    tagline: tagline
+  });
+})
 
 // add routes
 app.get('/urls.json', (req, res) => {
@@ -19,6 +36,15 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
